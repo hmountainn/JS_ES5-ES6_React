575c14a38e9019162ef1b723fe8c15601cae3d1a
hmountainn <ghkdwltks123@naver.com>
2021-04-15 16:21:49 +0900

    명명규칙 지켜서 가져오기

--- js/ex1.js
+++ js/ex1.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-
-    var Test = window.document.getElementById("submit-button")
+    var section = window.document.getElementById("ex1")
+    var Test = section.getElementsByClassName("submit-button")[0]; //섹션중에 submit-button중이 0번째
 
     Test.onclick = function(){
         console.log("test")
