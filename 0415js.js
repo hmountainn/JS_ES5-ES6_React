faf7deae43ced6a9f49eb3c4755f901d2c6f73d3
hmountainn <ghkdwltks123@naver.com>
2021-04-15 15:52:44 +0900

    명명규칙을 지키기위해 직접 가져와서 쓰기(getElementById)

--- 0415js.js
+++ 0415js.js
@@ -1,6 +1,9 @@
 //윈도우 로드하고 onclick발동 한번에 모으기
 window.onload = function(){
-    title.onclick = function onPrompt(){
+    //명명규칙을 지키기위해서 직접 가져와서 쓰기
+    var asideTitle = window.document.getElementById("aside-title")
+
+    asideTitle.onclick = function onPrompt(){
         var x = prompt('x:');
         var y = prompt('y:');
         x = parseInt(x);
