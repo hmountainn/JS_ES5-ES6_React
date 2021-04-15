91fef4f3138bfe9a8650008e558f86c7ee5b12a3
hmountainn <ghkdwltks123@naver.com>
2021-04-15 18:01:40 +0900

    계산결과 구현

--- js/ex1.js
+++ js/ex1.js
@@ -1,8 +1,27 @@
 window.onload = function(){
-    var section = window.document.getElementById("ex1")
-    var Test = section.getElementsByClassName("submit-button")[0]; //섹션중에 submit-button중이 0번째
+    var section = window.document.getElementById("ex2");
+    var button = section.getElementsByClassName("submit-button")[0];
 
-    Test.onclick = function(){
-        console.log("test")
-    } 
+    var x = section.getElementsByClassName("x")[0];
+    var y = section.getElementsByClassName("y")[0];
+    var result = section.getElementsByClassName("result")[0];
+
+    button.onclick = function(){
+        
+        x = parseInt(x.value);
+        y = parseInt(y.value);
+
+        result.innerText = x+y;
+    }
 }
+
+
+
+// window.onload = function(){
+//     var section = window.document.getElementById("ex1");
+//     var Test = section.getElementsByClassName("submit-button")[0]; //섹션중에 submit-button중이 0번째
+
+//     Test.onclick = function(){
+//         console.log("test");
+//     } 
+// };
