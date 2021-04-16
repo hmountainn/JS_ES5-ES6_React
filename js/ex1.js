4166c681760c22a4ccb998d7e9b392cf53303118
hmountainn <ghkdwltks123@naver.com>
2021-04-16 15:12:24 +0900

    계산기 수정

--- js/ex1.js
+++ js/ex1.js
@@ -1,18 +1,17 @@
 window.onload = function(){
     var section = window.document.getElementById("ex2");
     var button = section.getElementsByClassName("submit-button")[0];
-
+    var x = section.getElementsByClassName("x")[0];
+    var y = section.getElementsByClassName("y")[0];
+    var result = section.getElementsByClassName("result")[0];
 
 
     button.onclick = function(){
-        var x = section.getElementsByClassName("x")[0];
-        var y = section.getElementsByClassName("y")[0];
-        var result = section.getElementsByClassName("result")[0];
+        
+        var a = parseInt(x.value);
+        var b = parseInt(y.value);
 
-        x = parseInt(x.value);
-        y = parseInt(y.value);
-
-        result.innerText = x+y;
+        result.innerText = a+b;
     }
 }
 
