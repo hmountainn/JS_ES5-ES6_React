4660b8a636c451469e63c1eabad2911e6dab8047
hmountainn <ghkdwltks123@naver.com>
2021-04-16 16:05:28 +0900

    누르고 3초후에 실행

--- js/ex3.js
+++ js/ex3.js
@@ -6,9 +6,12 @@ window.addEventListener("load", function(){
     var button = section.querySelector(".btn");
 
     button.onclick = function(){
-        //누를때마다 1씩 줄어들기
+        // window.setTimeout(로직, 시간(3000:3초))
+        //3초후에 실행
+        window.setTimeout(function(){
         var num = parseInt(span.innerText);
         num--;
         span.innerText = num;
+        }, 3000);
     }
 });
