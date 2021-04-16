bc6d05ef0f18cc44958dfbbedf846cdc46029ba2
hmountainn <ghkdwltks123@naver.com>
2021-04-16 15:51:33 +0900

    querySelector로 변경

--- js/ex3.js
+++ js/ex3.js
@@ -1,7 +1,9 @@
 window.addEventListener("load", function(){
-    var section = window.document.getElementById("ex2");
-    var span = section.getElementsByClassName("span")[0];
-    var button = section.getElementsByClassName("btn")[0];
+    //querySelector로 변경
+    var section = document.querySelector("#ex3")
+
+    var span = section.querySelector(".span");
+    var button = section.querySelector(".btn");
 
     button.onclick = function(){
         console.log("test")
