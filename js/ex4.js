76255c8330992fad8c280800a13d6526fb288f82
hmountainn <ghkdwltks123@naver.com>
2021-04-16 17:39:33 +0900

    버튼누르면 계속 튀어나오게 하기

--- js/ex4.js
+++ js/ex4.js
@@ -1,12 +1,16 @@
 window.addEventListener("load", function(){
     var section = document.querySelector("#ex4")
 
-    var container = section.querySelector(".container");
     var boxes = section.querySelectorAll(".box");
     var button = section.querySelector("input[value='click']");
 
-    button.onclick = function(){
-        //버튼 누르면 파란색원 위치바꾸기
-        boxes[0].style.left = "100px";
+    button.onclick = function(){    
+        //버튼 누르면 계속 튀어나오기
+        // window.setInterval(로직, 시간(3000:3초))
+        var num = 0;
+        window.setInterval(function(){
+            num = num+1;
+            boxes[0].style.left = num+"px";
+        }, 17);
     }
 });
