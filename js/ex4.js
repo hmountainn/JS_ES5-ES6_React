593198f1934040a71766ff8eae82776a637e13ff
hmountainn <ghkdwltks123@naver.com>
2021-04-19 15:06:45 +0900

    css스타일 들고오기

--- js/ex4.js
+++ js/ex4.js
@@ -4,15 +4,24 @@ window.addEventListener("load", function(){
     var boxes = section.querySelectorAll(".box");
     var button = section.querySelector("input[value='click']");
 
-    button.onclick = function(){    
-        //버튼 누르면 계속 튀어나오기
-        var left = 0;
-        var time = window.setInterval(function(){
-            left++;
-            boxes[0].style.left = left+"px";
-            if(left == 400){ //박스 끝에서 멈추기
-                clearInterval(time);
-            }
-        }, 17);
+    button.onclick = function(){
+        
+        var box = boxes[0];
+        //css 스타일을 들고오기(box1)
+        let boxStyle = window.getComputedStyle(box);
+        var left = boxStyle.getPropertyValue("left");
+
+        console.log(left);
+
+
+        // //버튼 누르면 계속 튀어나오기
+        // var left = 0;
+        // var time = window.setInterval(function(){
+        //     left++;
+        //     boxes[0].style.left = left+"px";
+        //     if(left == 400){ //박스 끝에서 멈추기
+        //         clearInterval(time);
+        //     }
+        // }, 17);
     }
 });
