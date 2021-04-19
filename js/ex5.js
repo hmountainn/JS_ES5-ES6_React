0894dcc9b46f80ae4ee06f4a60ed30592bfa66b8
hmountainn <ghkdwltks123@naver.com>
2021-04-19 16:20:13 +0900

    사진 추가

--- js/ex5.js
+++ js/ex5.js
@@ -1,12 +1,20 @@
 window.addEventListener("load", function(){
     var section = document.querySelector("#ex5");
 
+    var container = section.querySelector(".container");
     var btnAdd = section.querySelector(".btn-add");
     var btnDel = section.querySelector(".btn-del");
     var btnClone = section.querySelector(".btn-clone");
 
     btnAdd.onclick = function(){
-        console.log("add")
+        //1. 엘리먼트 객체를 생성하기
+        var img = document.createElement("img");
+        //2. 엘리먼트 객체의 속성 설정하기
+        img.src = "../images/1.jpg";
+        
+        //3. 엘리먼트 객체를 문서에 추가하기
+        container.appendChild(img);
+
     }
 
     btnDel.onclick = function(){
