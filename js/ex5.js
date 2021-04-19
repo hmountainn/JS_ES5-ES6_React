cbed7035ca49de85aea8765bcbe4b51749cc1980
hmountainn <ghkdwltks123@naver.com>
2021-04-19 16:37:53 +0900

    원 추가하기

--- js/ex5.js
+++ js/ex5.js
@@ -7,14 +7,25 @@ window.addEventListener("load", function(){
     var btnClone = section.querySelector(".btn-clone");
 
     btnAdd.onclick = function(){
-        //1. 엘리먼트 객체를 생성하기
-        var img = document.createElement("img");
-        //2. 엘리먼트 객체의 속성 설정하기
-        img.src = "../images/1.jpg";
+        // //1. 엘리먼트 객체를 생성하기
+        // var img = document.createElement("img");
+        // //2. 엘리먼트 객체의 속성 설정하기
+        // img.src = "../images/1.jpg";
         
-        //3. 엘리먼트 객체를 문서에 추가하기
-        container.appendChild(img);
+        // //3. 엘리먼트 객체를 문서에 추가하기
+        // container.appendChild(img);
 
+        //1. 엘리먼트 객체를 생성하기
+        var div = document.createElement("div");
+        
+        //2. 엘리먼트 객체의 속성 설정하기
+        div.style.backgroundColor = "blue";
+        div.style.width = "100px";
+        div.style.height = "100px";
+        div.style.borderRadius = "50px";
+
+        //3. 엘리먼트 객체를 문서에 추가하기
+        container.appendChild(div);
     }
 
     btnDel.onclick = function(){
