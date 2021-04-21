c882bf1b127ed90733b0cdb8a99f46cd62dd9d38
hmountainn <ghkdwltks123@naver.com>
2021-04-21 15:43:43 +0900

    ex6 이벤트 객체 target속성 준비

---
+++ js/ex6.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(){
+    var section = document.querySelector("#ex6");
+
+    var container = section.querySelector(".container");
+    var btnDel = section.querySelector(".btn-del");
+    var btnClone = section.querySelector(".btn-clone");
+    var btnChange = section.querySelector(".btn-change");
+
+    btnDel.onclick = function(){
+        console.log("t");
+    }
+});
