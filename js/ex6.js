551ca4865c39005580a4eb5435553ef387616bdb
hmountainn <ghkdwltks123@naver.com>
2021-04-22 15:20:17 +0900

    이벤트 버블링 사용

--- js/ex6.js
+++ js/ex6.js
@@ -8,14 +8,24 @@ window.addEventListener("load", function(){
 
     var selected = null;
 
-    var boxes = container.querySelectorAll(".box");
-    //박스 클릭하면 selected에 넣기
-    for(let i=0; i<boxes.length; i++){
-        boxes[i].onclick = function(){
-            selected = boxes[i];
-            console.log(selected);
-        }
+    // var boxes = container.querySelectorAll(".box");
+    // //박스 클릭하면 selected에 넣기
+    // for(let i=0; i<boxes.length; i++){
+    //     boxes[i].onclick = function(){
+    //         selected = boxes[i];
+    //         console.log(selected);
+    //     }
+    // }
+
+    //이벤트 버블링과 event.target속성사용
+    container.onclick = function(e){
+        if(e.target.className != "box") //box아닌건 그냥 넘어가
+            return;
+
+        console.log(e.target);
+        selected = e.target;
     }
+
     //selected 삭제
     btnDel.onclick = function(){
         if(selected != null)
