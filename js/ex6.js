404e3c6d7222ab2180d5ee1c8a5d676ccf47554d
hmountainn <ghkdwltks123@naver.com>
2021-04-22 12:52:44 +0900

    선택한 박스 삭제

--- js/ex6.js
+++ js/ex6.js
@@ -6,7 +6,19 @@ window.addEventListener("load", function(){
     var btnClone = section.querySelector(".btn-clone");
     var btnChange = section.querySelector(".btn-change");
 
+    var selected = null;
+
+    var boxes = container.querySelectorAll(".box");
+    //박스 클릭하면 selected에 넣기
+    for(let i=0; i<boxes.length; i++){
+        boxes[i].onclick = function(){
+            selected = boxes[i];
+            console.log(selected);
+        }
+    }
+    //selected 삭제
     btnDel.onclick = function(){
-        console.log("t");
+        if(selected != null)
+            selected.remove();
     }
 });
