562f6a34e81baeb28ff7a581fd51339eba2dca6d
hmountainn <ghkdwltks123@naver.com>
2021-04-26 14:51:05 +0900

    ex7 아코디언 js 준비

---
+++ js/ex7.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+    var section = document.querySelector("#ex7");
+
+    var accordion = section.querySelector(".accordion");
+    accordion.onclick = function(e){
+        console.log("t");
+    }
+
+});
