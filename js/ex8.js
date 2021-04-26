ad38ea6d07ebd87de8cb81047bdbe2b8802f12ce
hmountainn <ghkdwltks123@naver.com>
2021-04-26 16:29:39 +0900

    각각 버튼 구체적으로 구현

--- js/ex8.js
+++ js/ex8.js
@@ -3,12 +3,22 @@ window.addEventListener("load",function(){
 
     var product = section.querySelector(".product");
     product.onclick = function(e){
-        if(!e.target.classList.contains("up") &&
-        !e.target.classList.contains("down") &&
-        !e.target.classList.contains("current"))
+        var item = e.target;
+        if(!item.classList.contains("up") &&
+        !item.classList.contains("down") &&
+        !item.classList.contains("current"))
             return;
 
-        console.log("gd") 
+        if(item.classList.contains("up")){
+            var input = item.parentElement.querySelector("input");
+            input.value = parseInt(input.value)+1;
+        }
+        else if(item.classList.contains("down")){
+            var input = item.parentElement.querySelector("input");
+            input.value = parseInt(input.value)-1;
+        }
+        else if(item.classList.contains("current")){
+            item.parentElement.style.border = "2px dotted #000";
+        }
     }
-
 });
