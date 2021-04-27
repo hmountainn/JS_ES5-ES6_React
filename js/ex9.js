1cada7203e1c111d0698ee1ec263d93a81ba72ae
hmountainn <ghkdwltks123@naver.com>
2021-04-27 15:21:56 +0900

    js파일 준비

---
+++ js/ex9.js
@@ -0,0 +1,9 @@
+window.addEventListener("load",function(){
+    var section = document.querySelector("#ex9");
+
+    var selButton = section.querySelector(".btn-sel");
+
+    selButton.onclick = function(e){
+        console.log("ttt");
+    }
+});
