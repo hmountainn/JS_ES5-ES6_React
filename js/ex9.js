21e0daa8dafb9200027ea34dea89eb3dacd270f6
hmountainn <ghkdwltks123@naver.com>
2021-04-27 15:55:39 +0900

    drag이벤트 구현

--- js/ex9.js
+++ js/ex9.js
@@ -1,9 +1,20 @@
 window.addEventListener("load",function(){
     var section = document.querySelector("#ex9");
 
+    var uploadBox = section.querySelector(".upload-box")
     var selButton = section.querySelector(".btn-sel");
     var fileButton = section.querySelector(".btn-file");
 
+    //darg이벤트
+    uploadBox.ondragenter = function(){
+        console.log("enter");
+    }
+
+    uploadBox.ondragleave = function(){
+        console.log("leave");
+    }
+
+    //가짜버튼 누르면 진짜업로드버튼 실행
     selButton.onclick = function(e){
         var event = new MouseEvent("click",{
             'view' : window,
