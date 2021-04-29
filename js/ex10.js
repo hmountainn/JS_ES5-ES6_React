a1ca637f52711b0ca08f34bc8ed65a944f8bffc2
hmountainn <ghkdwltks123@naver.com>
2021-04-29 15:17:17 +0900

    ex10 ajax활용 준비

---
+++ js/ex10.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",function(){
+    var requestButton = document.querySelector(".btn-request");
+
+    requestButton.onclick = function(){
+        console.log("클릭");
+    }
+
+})
