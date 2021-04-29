c6f3f84ba8b3ed5f309ba5f439370544d6829442
hmountainn <ghkdwltks123@naver.com>
2021-04-29 16:00:17 +0900

    ex10.txt 요청받아서 콘솔 출력

--- js/ex10.js
+++ js/ex10.js
@@ -2,7 +2,12 @@ window.addEventListener("load",function(){
     var requestButton = document.querySelector(".btn-request");
 
     requestButton.onclick = function(){
-        console.log("클릭");
+        var request = new XMLHttpRequest(); //요청도구
+
+        request.open("GET", "ex10.txt", false);
+        request.send(null);
+
+        console.log(request.responseText);
     }
 
 })
