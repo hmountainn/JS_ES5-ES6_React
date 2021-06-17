019f71b4ea3126c38a5c7bad470a275d120ab657
hmountainn <ghkdwltks123@naver.com>
2021-06-17 10:11:27 +0900

    javascript (ES6) 준비

---
+++ es6js/es6.js
@@ -0,0 +1,15 @@
+// Collection
+let set = new Set();
+set.add(5);
+set.add(5);
+set.add(5);
+set.add(5);
+set.add(6);
+
+console.log(set.size)
+
+let arr = [2,2,3,8,6,4,8,4,8,4,8,5,6,4,8,4,7];
+console.log(arr.length);
+
+let lotto = new Set(arr);
+console.log(lotto.size);
